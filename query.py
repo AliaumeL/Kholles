332e5c840dff89cc27e56f6269d03f4d81b10946
Aliaume Lopez <aliaume.lopez@ens-cachan.fr>
2018-02-25 17:05:37 +0100

    On commence à avoir un truc raisonnable

--- query.py
+++ query.py
@@ -15,6 +15,7 @@ CONFIG = {
     "catDIR"  : "Catalogue",
     "semDIR"  : "Semaines",
     "fihDIR"  : "Fiches",
+    "reqDIR"  : "Request",
     "debug"   : False,
     "log"     : []
         }
@@ -31,7 +32,8 @@ def debugInfo (action, description):
 
         @return : string
     """
-    return "[INFO ] ({}) : {}".format (action, description)
+    if CONFIG["debug"] == True:
+        CONFIG["log"].append ("[INFO ] ({}) : {}".format (action, description))
 
 def debugError (action, description):
     """
@@ -44,7 +46,8 @@ def debugError (action, description):
 
         @return : string
     """
-    return "[ERROR] ({}) : {}".format (action, description)
+    CONFIG["log"].append ("[ERROR] ({}) : {}".format (action, description))
+
 
 
 def openFile (filename):
@@ -56,8 +59,7 @@ def openFile (filename):
 
         @return : string
     """
-    if CONFIG["debug"] == True:
-        CONFIG["log"].append (debugInfo ("OPENFILE", filename))
+    debugInfo ("OPENFILE", filename))
 
     return open (filename, "r")
         
@@ -65,7 +67,7 @@ def openFile (filename):
 DATABASE = yaml.load (open (CONFIG["tagfile"], "r"))
 
 
-def writeExercice (exoID):
+def fetchExercice (exoID):
     """
         Écrit textuellement 
         le contenu de l'exercice exoID
@@ -75,13 +77,63 @@ def writeExercice (exoID):
 
         @return : unit
     """
+    debugInfo ("EXOWRITE", "exoID : {}".format (exoID))
     return "".join(openFile ("{}/{}.tex".format (CONFIG["exoDIR"],
         exoID)).readlines ())
 
-def questionCours (semaine):
+def fetchCours (semaine):
     """
         Récupère les questions 
         de cours pour une semaine 
+        donnée 
     """
-    return yaml.load (open ("{}/{}.yaml".format (CONFIG["semDIR"], semaine),"r"))
+    debugInfo ("COURS", " questions de cours de « {} »".format (semaine))
+    with open ("{}/{}.yaml".format (CONFIG["semDIR"], semaine),"r") as f
+        return yaml.load (f)
 
+
+
+## TYPE REQUEST : STRING DICT
+# où les clefs sont exactement les clefs 
+# des métadonnées des exercices 
+
+def fetchRequest (reqname):
+    """ 
+        Charche une requête dans un 
+        fichier 
+    """
+    debugInfo ("LOADREQ", " requête {}".format (reqname))
+    with open ("{}/{}.yaml".format (CONFIG["reqDIR"], reqname),"r") as f
+        return yaml.load (f)
+
+def saveRequest (reqname, request):
+    """
+        Enregistre une requête dans 
+        un fichier 
+    """
+    debugInfo ("SAVEREQ", " requête {}".format (reqname))
+    with open ("{}/{}".format (CONFIG["reqDIR"], reqname), "w") as f:
+        yaml.dump (request, f, default_flow_style=False)
+
+def fetchExercices (request):
+    """
+        Fait une recherche d'exercices 
+        étant donné une requête passée 
+        en argument.
+
+        request : <Request> 
+            La requête à effectuer sur la DB
+
+        @return : iterator exoID
+    """
+    debugInfo ("DBREQ", "request : {}".format (request))
+
+    for exoID in DATABASE.keys ():
+        if requestValidate (request, exoID):
+            yield exoID
+    
+if __name__ == "__main__":
+    # TODO: ici ajouter l'interactivité pour 
+    # par exemple tester des requêtes, et 
+    # les enregistrer dans des fichiers 
+    print ("coucou")
